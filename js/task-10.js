01c37531326f60b5ec4391a3a652ae7dc22084e9
Jaciubek <67959983+Jaciubek@users.noreply.github.com>
2022-06-28 23:56:17 +0200

    Add files via upload
    poprawiono css w task 03 oraz poprawiono task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,27 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const divBoxes = document.querySelector("#boxes");
-const create = document.querySelector("button[data-create]");
-const destroy = document.querySelector("button[data-destroy]");
-const value = document.querySelector("#controls input");
+
+
+const inputNumber = document.querySelector("#controls input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
 
 
 
 function createBoxes(amount) {
-
-  amount = value.value;
-  const boxArray = [];
-  const box = document.createElement("div");
-
-  box.style.width = "30px";
-  box.style.height = "30px";
-  box.style.backgroundColor = getRandomHexColor();
-  boxArray.push(box);
-  divBoxes.append(box);
+  amount = inputNumber.value;
+  const box = [];
+  
+  for (let i = 0; i < amount; i++) {
+    let size = 30 + i * 10;
+    let color = getRandomHexColor();
+  
+    box.push(`<div style="height:${size}px ; width:${size}px ; background-color: ${color};"></div>`);
+  }
+  
+  const markup = box.join("");
+  return boxes.insertAdjacentHTML("afterbegin", markup);
 }
 function destroyBoxes() {
-  divBoxes.innerHTML = "";
-}
-create.addEventListener("click", createBoxes);
-destroy.addEventListener("click", destroyBoxes);
+ const boxAll = document.querySelectorAll('#boxes div');
+  boxAll.forEach(box => box.remove());
+  inputNumber.value = 0;
+};
